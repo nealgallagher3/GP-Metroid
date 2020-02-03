4635399df160ac6e70a7a8907b63345b9b895d97
Colin Hummel <co827967@ucf.edu>
2020-02-03 16:14:25 -0500

    Base Movement Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+}
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    public float speed;
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
     
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     
     void Update()
     {
-        
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
     }
 }
