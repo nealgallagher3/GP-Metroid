c910c4f35d6eef16ca9b4eca561bbd148cadacb6
Colin Hummel <co827967@ucf.edu>
2020-02-03 17:12:45 -0500

    2/3

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,30 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerMover : MonoBehaviour
 {
     private Rigidbody2D rb;
     private bool facingRight = true;
+    private int lifeValue = 100;
+
     public float speed;
+    public Text lifeText;
+    public Text endText;
+    public GameObject death;
 
     void Flip()
     {
         facingRight = !facingRight;
         transform.Rotate(0f, 180f, 0f);
     }
+
+    void gameOver()
+    {
+        if (lifeValue <= 0)
+        {
+            endText.text = "You lose!";
+            death.SetActive(false);
+        }
+    }
     
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lifeText.text = "Health: " + lifeValue.ToString();
+        endText.text = "";
 
     }
 
     
     void Update()
     {
+
+        
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
 
@@ -42,6 +61,19 @@ public class PlayerMover : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("Buff"))
+        {
+            speed += 10;
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            lifeValue -= 30;
+            lifeText.text = "Health: " + lifeValue.ToString();
+            other.gameObject.SetActive(false);
+            gameOver();
+
+        }
         if (other.gameObject.CompareTag("Load2"))
         {
             SceneManager.LoadScene("Level 2");
