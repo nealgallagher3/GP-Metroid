f1828748b9d3df1f7dd0ef8a6845eb4411c131f9
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-02-06 00:32:16 -0500

    PingPong

--- Assets/Scripts/PingPong.cs
+++ Assets/Scripts/PingPong.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PingPong : MonoBehaviour
 {
-   
+    public Transform startMarker;
+    public Transform endMarker;
+
+    public float speed;
+
+    private float startTime;
+    private float journeyLength;
     void Start()
     {
-        
+        startTime = Time.time;
+
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
     }
 
-   
+
     void Update()
     {
-        
+        float distCovered = (Time.time - startTime) * speed;
+
+        float fracJourney = distCovered / journeyLength;
+
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
     }
 }
